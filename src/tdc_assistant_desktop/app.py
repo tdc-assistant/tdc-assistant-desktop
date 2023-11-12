0de87ec43d350c7888725cc9ff1a4f4b68ecfe3a
Adam Carter <adam.clayton.carter@gmail.com>
2023-11-12 10:08:43 -0500

    Fix update_recent_chat_log to handle case where there are no chat logs in local storage

--- src/tdc_assistant_desktop/app.py
+++ src/tdc_assistant_desktop/app.py
@@ -4,6 +4,7 @@ from tdc_assistant_client.client import TdcAssistantClient
 from config.env import env
 
 from tasks import persist_chat_log, create_chat_completion_annotation
+from store import load_most_recent_chat_log
 
 
 def main():
@@ -15,6 +16,8 @@ def run_cli(client: TdcAssistantClient, controller: TdcAssistantGuiControllerV2)
         option = display_menu()
         if option == "1":
             scrape_public_chat_and_create_chat_completion(client, controller)
+        elif option == "2":
+            get_chat_completion_for_last_chat_log(client)
         elif option == "q":
             break
         else:
@@ -46,11 +49,39 @@ def scrape_public_chat_and_create_chat_completion(
         print("Failed to create annotation for `ChatLog`")
         return
 
-    print(chat_log_with_annotation)
+    for message in chat_log_with_annotation["messages"]:
+        for annotation in message["annotations"]:
+            if annotation["type"] == "CHAT_COMPLETION":
+                chat_completion = annotation["chatCompletion"]
+                if chat_completion is None:
+                    continue
+
+                parts = chat_completion["parts"]
+                if parts is None:
+                    continue
+                for part in parts:
+                    part_type = part["type"]
+                    print(f"[{part_type}]")
+                    print(part["content"])
+                    print()
+
+
+def get_chat_completion_for_last_chat_log(client: TdcAssistantClient):
+    chat_log = load_most_recent_chat_log()
+
+    if chat_log is None:
+        return
+
+    chat_log_completion = client.create_chat_completion_annotation(
+        message=chat_log["messages"][-1]
+    )
+
+    print(chat_log_completion)
 
 
 def display_menu() -> str:
     print("[1] Scrape public chat and create chat completion")
+    print("[2] Generate chat completion for last message")
     print("[Q] Quit")
     return input("Enter option: ").strip().lower()
 

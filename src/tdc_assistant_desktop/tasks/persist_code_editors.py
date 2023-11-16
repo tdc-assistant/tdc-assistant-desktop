e4a4708cff4ab9d7d4e5bc31e1c2a66469ec5f5b
Adam Carter <adam.clayton.carter@gmail.com>
2023-11-16 10:43:57 -0500

    update existing workspaces instead of creating new ones

--- src/tdc_assistant_desktop/tasks/persist_code_editors.py
+++ src/tdc_assistant_desktop/tasks/persist_code_editors.py
@@ -3,8 +3,6 @@ from tdc_assistant_client.client import TdcAssistantClient
 from tdc_assistant_gui_controller_v2.controller import TdcAssistantGuiControllerV2
 from tdc_assistant_client.domain import ChatLog
 
-from .fetch_most_recent_chat_log import fetch_most_recent_chat_log
-
 
 def persist_code_editors(
     client: TdcAssistantClient,
@@ -16,11 +14,19 @@ def persist_code_editors(
     if len(messages) == 0:
         raise Exception(f"No messages exist for ChatLog: '{chat_log['id']}'")
 
+    workspaces = chat_log["workspaces"]
+
     for board_number, editor in enumerate(controller.scrape_editor()):
-        client.create_workspace(
-            chat_log=chat_log,
-            board_number=board_number,
-            content=editor["content"],
-            # FIXME
-            workspace_type="CODE_EDITOR",
-        )
+        if board_number > len(workspaces):
+            client.create_workspace(
+                chat_log=chat_log,
+                board_number=board_number,
+                content=editor["content"],
+                # FIXME
+                workspace_type="CODE_EDITOR",
+            )
+        else:
+            client.update_workspace(
+                workspace=workspaces[board_number],
+                content=editor["content"],
+            )

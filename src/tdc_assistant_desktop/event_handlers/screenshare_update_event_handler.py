b51ca17bf14926485384ffa4cbd7e91f7cbcb06f
Adam Carter <adam.clayton.carter@gmail.com>
2023-11-23 11:38:24 -0500

    Add screenshare image capture observer and event handler

---
+++ src/tdc_assistant_desktop/event_handlers/screenshare_update_event_handler.py
@@ -0,0 +1,35 @@
+from tdc_assistant_gui_controller_v2.controller import TdcAssistantGuiControllerV2
+from tdc_assistant_client.client import TdcAssistantClient
+from tdc_assistant_client.domain import Message
+
+from tasks import fetch_most_recent_chat_log
+
+
+class ScreenshareUpdateEventHandler:
+    _client: TdcAssistantClient
+    _controller: TdcAssistantGuiControllerV2
+
+    def __init__(
+        self, client: TdcAssistantClient, controller: TdcAssistantGuiControllerV2
+    ):
+        self._client = client
+        self._controller = controller
+
+    def handle(self):
+        chat_log = fetch_most_recent_chat_log(self._client)
+
+        if chat_log is None:
+            return
+
+        for message in reversed(chat_log["messages"]):
+            if message["role"] == "user":
+                self._create_image_capture_for_message(message)
+                return
+
+    def _create_image_capture_for_message(self, message: Message):
+        screenshare = self._controller.scrape_screenshare()
+
+        if screenshare is not None:
+            self._client.create_image_capture_annotation(
+                message=message, image_url=screenshare["image_url"]
+            )
---
+++ src/tdc_assistant_desktop/observers/screenshare_observer.py
@@ -0,0 +1,68 @@
+from typing import Optional
+
+from datetime import datetime, timezone, timedelta
+
+from tdc_assistant_client.client import TdcAssistantClient
+from tdc_assistant_client.domain import ImageCaptureAnnotation
+from tdc_assistant_gui_controller_v2.controller import TdcAssistantGuiControllerV2
+
+from domain import Event
+
+from .base_observer import BaseObserver
+
+
+IMAGE_CAPTURE_INTERVAL_IN_MINUTES = 3
+
+
+class ScreenshareObserver(BaseObserver):
+    _client: TdcAssistantClient
+    _controller: TdcAssistantGuiControllerV2
+
+    def __init__(
+        self, client: TdcAssistantClient, controller: TdcAssistantGuiControllerV2
+    ):
+        super().__init__(client, controller)
+
+    def _fetch_most_recent_image_capture_annotation(
+        self,
+    ) -> Optional[ImageCaptureAnnotation]:
+        chat_log = self._fetch_most_recent_chat_log()
+
+        if chat_log is None:
+            return None
+
+        for message in reversed(chat_log["messages"]):
+            for annotation in reversed(message["annotations"]):
+                if annotation["type"] == "IMAGE_CAPTURE":
+                    return annotation
+
+        return None
+
+    def poll(self) -> Optional[Event]:
+        """
+        Generates a "screenshare-update" event when either
+        (1) The screenshare window is visible and no image capture exists
+        (2) Enough time has passed between now and the last image capture that was created
+        """
+        if not self._controller.is_screenshare_window_open():
+            return None
+
+        most_recent_image_capture_annotation = (
+            self._fetch_most_recent_image_capture_annotation()
+        )
+
+        if most_recent_image_capture_annotation is None:
+            # Screenshare window is open but an image capture has not been created yet
+            return {"name": "screenshare-update"}
+
+        should_update_screenshare = most_recent_image_capture_annotation[
+            "createdAt"
+        ] < datetime.now(timezone.utc) - timedelta(
+            minutes=IMAGE_CAPTURE_INTERVAL_IN_MINUTES
+        )
+
+        if should_update_screenshare:
+            # Enough time has passed since the last image capture
+            return {"name": "screenshare-update"}
+
+        return None

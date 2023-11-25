7a228c84a5e971db8894b4a4edce6db578f22fb7
Adam Carter <adam.clayton.carter@gmail.com>
2023-11-25 09:38:24 -0500

    Add logging to chat_completion_ready_observer

--- src/tdc_assistant_desktop/observers/chat_completion_ready_observer.py
+++ src/tdc_assistant_desktop/observers/chat_completion_ready_observer.py
@@ -16,13 +16,25 @@ class ChatCompletionReadyObserver(BaseObserver):
         super().__init__(client, controller)
 
     def poll(self) -> Optional[Event]:
+        # (1) Fetch most recent chat log
         chat_log = super()._fetch_most_recent_chat_log()
 
         if chat_log is None:
             return None
 
+        # (2) Find most recently approved chat completion
+
+        most_recent_chat_completion_start = self._logger.log(
+            "Started finding most recent chat completion"
+        )
         most_recent_approved_chat_completion = (
             find_most_recent_approved_chat_completion(chat_log)
+        )
+        most_recent_chat_completion_end = self._logger.log(
+            "Finished finding most recent chat completion"
+        )
+        self._logger.log_elapsed_time(
+            most_recent_chat_completion_start, most_recent_chat_completion_end
         )
 
         if most_recent_approved_chat_completion is None:

7c5a47c76c62062c2ab573391cdb7bb06df6c567
Adam Carter <adam.clayton.carter@gmail.com>
2023-11-29 17:48:06 -0500

    Add in retry loop when chat completion approval fails

--- src/tdc_assistant_desktop/commands/create_chat_completion_mock_controller.py
+++ src/tdc_assistant_desktop/commands/create_chat_completion_mock_controller.py
@@ -1,3 +1,7 @@
+from pprint import pprint
+
+from time import sleep
+
 from tdc_assistant_gui_controller_v2.controller import TdcAssistantGuiControllerV2
 from tdc_assistant_client.client import TdcAssistantClient
 
@@ -6,15 +10,31 @@ def create_chat_completion_mock_controller(
     client: TdcAssistantClient, controller: TdcAssistantGuiControllerV2
 ):
     chat_log_before = client.create_chat_log(customer_name="Demo", raw_text="")
-    client.create_messages(
-        messages=[
-            {
-                "chat_log_id": chat_log_before["id"],
-                "content": "Hello there!",
-                "role": "user",
-            }
-        ]
+    chat_log_before = client.get_chat_log(
+        chat_log_id="0d1ef06a-7db4-4197-bcb0-4306c8059dca"
     )
-    client.create_chat_completion(chat_log=chat_log_before)
-    chat_log_after = client.get_chat_log(chat_log_id=chat_log_before["id"])
-    print(chat_log_after)
+    # client.create_messages(
+    #     messages=[
+    #         {
+    #             "chat_log_id": chat_log_before["id"],
+    #             "content": "Hello there!",
+    #             "role": "user",
+    #         }
+    #     ]
+    # )
+    chat_completion = client.create_chat_completion(chat_log=chat_log_before)
+    print(chat_completion)
+    chat_completion = client.get_chat_log(chat_log_id=chat_log_before["id"])[
+        "chatCompletions"
+    ][-1]
+
+    while True:
+        try:
+            result = client.request_chat_completion_approval(
+                chat_completion=chat_completion
+            )
+            break
+        except:
+            print("Retrying....")
+            sleep(5)
+    pprint(result)

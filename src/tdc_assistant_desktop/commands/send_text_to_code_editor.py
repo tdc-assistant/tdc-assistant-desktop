0183b599205eb7ba748ea1250d6903bc4ec97e62
Adam Carter <adam.clayton.carter@gmail.com>
2023-12-09 14:29:36 -0500

    Update send text to editor command

--- src/tdc_assistant_desktop/commands/send_text_to_code_editor.py
+++ src/tdc_assistant_desktop/commands/send_text_to_code_editor.py
@@ -1,8 +1,41 @@
 from tdc_assistant_gui_controller_v2.controller import TdcAssistantGuiControllerV2
 from tdc_assistant_client.client import TdcAssistantClient
 
+java_program_text: str = """
+public class Counter {
+
+    // Private components:
+    private int count; // The current count.
+
+    public Counter () {
+        this.count = 0;
+    }
+
+    public void incrementCount () {
+        this.count++;
+    }
+
+    public int getCount () {
+        return this.count;
+    }
+}
+"""
+
+python_program_text = """
+class Counter:
+    def __init__(self):
+        self.count = 0
+
+    def increment_count(self):
+        self.count += 1
+
+    def get_count(self):
+        return self.count
+"""
+
 
 def send_text_to_code_editor(
     client: TdcAssistantClient, controller: TdcAssistantGuiControllerV2
 ):
-    controller.send_text_to_code_editor("Java", 1, "Hello world!")
+    # controller.send_text_to_code_editor("Python", 1, python_program_text)
+    controller.send_text_to_code_editor("Java", 1, java_program_text)

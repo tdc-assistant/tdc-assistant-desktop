21423e997d8e354274796e207e632144438b07d9
Adam Carter <adam.clayton.carter@gmail.com>
2023-12-12 14:29:54 -0500

    Add exception handling for window exception

--- src/tdc_assistant_desktop/commands/run.py
+++ src/tdc_assistant_desktop/commands/run.py
@@ -1,6 +1,7 @@
 from typing import Union, List
 
 from tdc_assistant_gui_controller_v2.controller import TdcAssistantGuiControllerV2
+from tdc_assistant_gui_controller_v2.windows import WindowException
 from tdc_assistant_client.client import TdcAssistantClient
 from tdc_assistant_session_request_controller.controller import (
     controller as session_request_controller,
@@ -43,6 +44,9 @@ def run(client: TdcAssistantClient, controller: TdcAssistantGuiControllerV2) -> 
     chat_log = initialize_session.execute(fetch_most_recent_chat_log(client))
     i = 0
     while True:
-        chat_log = check_public_chat_task.execute(chat_log)
-        chat_log = tasks[i].execute(chat_log)
-        i = (i + 1) % len(tasks)
+        try:
+            chat_log = check_public_chat_task.execute(chat_log)
+            chat_log = tasks[i].execute(chat_log)
+            i = (i + 1) % len(tasks)
+        except WindowException:
+            break
